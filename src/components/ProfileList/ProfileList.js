786cdf17eab1cb7f3f02f4c724102af44f9f14cd
Yevhenii Malinovskiy <118572078+Travellin91@users.noreply.github.com>
2023-10-13 15:04:40 +0300

    Update ProfileList.js

--- src/components/ProfileList/ProfileList.js
+++ src/components/ProfileList/ProfileList.js
@@ -11,7 +11,7 @@ import {
   StatsData,
 } from './ProfileList.styled';
 
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = "https://65290dd855b137ddc83e2108.mockapi.io/users";
 
 export const ProfileList = () => {
   const [data, setData] = useState([]); // Масив усіх контактів
